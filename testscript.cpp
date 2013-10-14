035ad353551dfe729b3d758fe7d5a98e16ce85fb
Matthew <matty.beckett@btinternet.com>
2013-10-14 09:33:23 +0100

    Update testscript.cpp

--- testscript.cpp
+++ testscript.cpp
@@ -1,6 +1,19 @@
 /* Test C++ script to emulate the NMEA parser 
    in the tracker code for user-inputted NMEA
-   instead of serial data.                      
+   instead of serial data.         
+   
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details. */
+
+   
 */
 
 
