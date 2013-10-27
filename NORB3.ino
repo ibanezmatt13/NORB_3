e1bba5c5e5d35bcb471e12841bada568f4f123b9
Matthew <matty.beckett@btinternet.com>
2013-10-27 15:46:22 +0000

    Update NORB3.ino

--- NORB3.ino
+++ NORB3.ino
@@ -1,3 +1,27 @@
+/*
+ NORB 3 HAB Tracker
+ 
+ By Matthew Beckett
+ 
+ Thanks and credits :
+ 
+ Massive thanks to Steve Smith, David Akerman, Anthony Stirk, Chris Stubbs, Phil Heron, Ed Moore, mfa298, and all other
+ brilliant people who assisted in the creation of this code.
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ See <http://www.gnu.org/licenses/>.
+ */
+ 
+ 
 #include <util/crc16.h>
 #include <SHT1x.h>
 
