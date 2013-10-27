d25e05835c6cf979cd4ddd27a689be89a8dd8d1e
Matthew <matty.beckett@btinternet.com>
2013-10-27 07:43:48 +0000

    Update NORB3.ino

--- NORB3.ino
+++ NORB3.ino
@@ -1,13 +1,6 @@
 /*  Main code for NORB_v3 AVR HAB Tracker
     Copyright (C) 2013 Matthew Beckett
 
-    Credits:
-      	
-      	mfa298_M1ARI provided invaluable help in the understanding of how to create the code
-      	Functions for setting/checking flightmode and sending to NTX2 evolved from Anthony Stirk (M0UPU) on UKHAS Wiki
-      	A special thanks to Ed Moore for providing the massive encouragement and general assistance in the making of this code
-      	Thanks to the many people on #highaltitude UKHAS who assisted in all other aspects of assistance.
-
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
@@ -17,15 +10,12 @@
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details. */
-    
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+
 #include <util/crc16.h>
 
 #define RADIOPIN 9
 #define LED_1 5
-#define LED_2 6
+#define LED_2 6  
 
 // this command will set flight mode
 uint8_t setNav[] = {
@@ -33,31 +23,10 @@ uint8_t setNav[] = {
     0x05, 0x00, 0xFA, 0x00, 0xFA, 0x00, 0x64, 0x00, 0x2C, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0xDC };
 
-
-// calculates and returns checksum for datastring
-uint16_t gps_CRC16_checksum (char *string)
-{
-  size_t i;
-  uint16_t crc;
-  uint8_t c;
- 
-  crc = 0xFFFF;
- 
-  // Calculate checksum ignoring the first two $s
-  for (i = 2; i < strlen(string); i++)
-  {
-    c = string[i];
-    crc = _crc_xmodem_update (crc, c);
-  }
- 
-  return crc;
-}  
- 
-
    
 void rtty_txstring (char * string)
 {
-
+ 
  /* function to send a each character
    of the string to the rtty_txbyte().
    Each character is one byte (8 bits)
@@ -87,13 +56,13 @@ void rtty_txbyte (char c)
  
   rtty_txbit (0); // Start bit
  
-  // Send bits for for char LSB first	
+  // Send bits for for char LSB first        
  
   for (i=0;i<7;i++) // Change this here 7 or 8 for ASCII-7 / ASCII-8
   {
     if (c & 1) rtty_txbit(1); 
  
-    else rtty_txbit(0);	
+    else rtty_txbit(0);        
  
     c = c >> 1;
  
@@ -102,7 +71,6 @@ void rtty_txbyte (char c)
   rtty_txbit (1); // Stop bit
   rtty_txbit (1); // Stop bit
 }
- 
  
  
  
@@ -117,7 +85,6 @@ void rtty_txbit (int bit)
                             
 } 
  
-
  
 // function to convert latitude into decimal degrees
 int check_latitude(char* latitude, char* ind, float* new_latitude)
@@ -129,12 +96,12 @@ int check_latitude(char* latitude, char* ind, float* new_latitude)
     result+=(latitude[0]-'0')*10;
     result+=(latitude[1]-'0');
     result+=(strtod(&latitude[2],NULL) /60);
-	
+        
   if (strcmp(ind,"S")==0)
   {
     result*=-1;
   }
-	
+        
   *new_latitude = result;
   return 1;
   }
@@ -165,7 +132,7 @@ int check_longitude(char* longitude, char* ind, float* new_longitude)
   {
     result*=-1;
   }
-	
+        
   *new_longitude = result;
   return 1;
   }
@@ -183,6 +150,8 @@ int parse_NMEA(char* mystring, int flightmode)
   int counter = 0; // sentence id
   float new_latitude;
   float new_longitude;
+  char new_lat[12];
+  char new_lon[12];
   char time[12];
   int time_length;
   char identifier[7];
@@ -205,14 +174,12 @@ int parse_NMEA(char* mystring, int flightmode)
     // check for the identifer being invalid
     if (strncmp(identifier, "$GPGGA", 6) != 0)
     {
-      printf("Invalid NMEA String...1\n");
       return 0;
     }
     
     // check for a valid lock
     if (lock == 0)
     {
-      printf("No Lock...\n");
       return 0;
     }
     
@@ -225,7 +192,6 @@ int parse_NMEA(char* mystring, int flightmode)
     }
     else if (time_length < 6)
     {  
-      printf("Invalid NMEA String...2\n");
       return 0;
     }
     
@@ -243,13 +209,15 @@ int parse_NMEA(char* mystring, int flightmode)
     // if check_longitude() failed
     if (check_longitude_error == -1)
     {
-      printf("Invalid NMEA String...4\n");
       return 0;
     }
     
+    dtostrf(new_latitude,9,6,new_lat);
+    dtostrf(new_longitude,9,6,new_lon);
+    
     
     // pull everything together into a datastring and print
-    sprintf(datastring, "%s,%s,%d,%f,%s,%f,%s,%d,%s,%d,%s", identifier, time, counter, new_latitude, north_south, new_longitude, east_west, lock, flightmode, satellites, altitude);
+    sprintf(datastring, "%s,%s,%d,%s,%s,%s,%s,%d,%s,%d,%s", identifier, time, counter, new_lat, north_south, new_lon, east_west, lock, flightmode, satellites, altitude);
     unsigned int CHECKSUM = gps_CRC16_checksum(datastring);  // Calculates the checksum for this datastring
     char checksum_str[7];
     sprintf(checksum_str, "*%04X\n", CHECKSUM);
@@ -261,7 +229,23 @@ int parse_NMEA(char* mystring, int flightmode)
     return 0;
   }
 }
-  
+
+
+ 
+void setupGPS()
+{
+  Serial.println("$PUBX,40,GLL,0,0,0,0*5C");
+  delay(1000);
+  Serial.println("$PUBX,40,GSA,0,0,0,0*4E");
+  delay(1000);
+  Serial.println("$PUBX,40,RMC,0,0,0,0*47");
+  delay(1000);
+  Serial.println("$PUBX,40,GSV,0,0,0,0*59");
+  delay(1000);
+  Serial.println("$PUBX,40,VTG,0,0,0,0*5E");
+  delay(1000);
+}
+ 
 // Send a byte array of UBX protocol to the GPS
 void sendUBX(uint8_t *MSG, uint8_t len) {
   for(int i=0; i<len; i++) {
@@ -270,6 +254,23 @@ void sendUBX(uint8_t *MSG, uint8_t len) {
   Serial.println();
 }
 
+uint16_t gps_CRC16_checksum (char *string)
+{
+  size_t i;
+  uint16_t crc;
+  uint8_t c;
+ 
+  crc = 0xFFFF;
+ 
+  // Calculate checksum ignoring the first two $s
+  for (i = 2; i < strlen(string); i++)
+  {
+    c = string[i];
+    crc = _crc_xmodem_update (crc, c);
+  }
+ 
+  return crc;
+}    
 
 int getUBX_ACK(uint8_t *MSG) {
   uint8_t b;
@@ -278,16 +279,16 @@ int getUBX_ACK(uint8_t *MSG) {
   unsigned long startTime = millis();
  
   // Construct the expected ACK packet    
-  ackPacket[0] = 0xB5;	// header
-  ackPacket[1] = 0x62;	// header
-  ackPacket[2] = 0x05;	// class
-  ackPacket[3] = 0x01;	// id
-  ackPacket[4] = 0x02;	// length
+  ackPacket[0] = 0xB5;        // header
+  ackPacket[1] = 0x62;        // header
+  ackPacket[2] = 0x05;        // class
+  ackPacket[3] = 0x01;        // id
+  ackPacket[4] = 0x02;        // length
   ackPacket[5] = 0x00;
-  ackPacket[6] = MSG[2];	// ACK class
-  ackPacket[7] = MSG[3];	// ACK id
-  ackPacket[8] = 0;		// CK_A
-  ackPacket[9] = 0;		// CK_B
+  ackPacket[6] = MSG[2];        // ACK class
+  ackPacket[7] = MSG[3];        // ACK id
+  ackPacket[8] = 0;                // CK_A
+  ackPacket[9] = 0;                // CK_B
  
   // Calculate the checksums
   for (uint8_t i=2; i<8; i++) {
@@ -317,65 +318,56 @@ int getUBX_ACK(uint8_t *MSG) {
         ackByteID++;
       } 
       else {
-        ackByteID = 0;	// Reset and look again, invalid order
+        ackByteID = 0;        // Reset and look again, invalid order
       }
  
     }
   }
 }
-
-// disables all NMEA sentences except $GPGGA
-void setupGPS()
-{
-  Serial.println("$PUBX,40,GLL,0,0,0,0*5C");
-  delay(1000);
-  Serial.println("$PUBX,40,GSA,0,0,0,0*4E");
-  delay(1000);
-  Serial.println("$PUBX,40,RMC,0,0,0,0*47");
-  delay(1000);
-  Serial.println("$PUBX,40,GSV,0,0,0,0*59");
-  delay(1000);
-  Serial.println("$PUBX,40,VTG,0,0,0,0*5E");
-  delay(1000);
-}
-
  
 void setup()
 {
-  Serial.begin(9600); //begin communication with GPS
+  Serial.begin(9600);
   pinMode(RADIOPIN, OUTPUT);
   pinMode(LED_1, OUTPUT);
   pinMode(LED_2, OUTPUT);
-  digitalWrite(LED_1, HIGH); //turn LED 1 on
   setupGPS();
 }
-
+ 
 void loop()
 {
+  static char datastring[100];
   char character;
-  char string[90];
-  int n = 0;
+  static int n = 0;
   int flightmode = 0;
-  
-  character = Serial.read(); //read character
-  
-  if (character == '$'){ //if we read a $
+ 
+  if (Serial.available()){
+    character = Serial.read();
     
-    while(character != '\n'){ //while we've not reached the end of the data
-      string[n] = character; //store it at the nth position in string
-      n++; //increment n by 1
-      character = Serial.read(); //read another character
-      
-      if (n >= 88){ //if we received 88 or more 
-        break; //break this loop
-      }
-      if (character == '\n'){ //if we got to the end of the data
-        sendUBX(setNav, sizeof(setNav)/sizeof(uint8_t));
-        flightmode = getUBX_ACK(setNav);
-        parse_NMEA(string, flightmode);
-        memset(string,0,90); //empty string ready for reloop
-      }
+    if (character == '$'){
+      n = 0;
+      datastring[n] = character;
     }
+    
+    else if (character == '\n'){
+      datastring[n] = '\0';
+      flightmode = 0;
+      sendUBX(setNav, sizeof(setNav)/sizeof(uint8_t));
+      flightmode = getUBX_ACK(setNav);
+      parse_NMEA(datastring, flightmode);
+      n = 0;
+      flightmode = 0;
+    }
+    
+    else if (n > 0){
+      datastring[n] = character;
+    }
+    
+    if (n >= 98){
+      n = 0;
+    }
+    
+    n++;
+    
   }
 }
-  
